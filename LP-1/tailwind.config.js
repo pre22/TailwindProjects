d554a3524ab351321d9464fdd747ab7e0baecb56
Precious Chika <43546663+pre22@users.noreply.github.com>
2023-05-06 10:31:16 +0100

    added config

--- LP-1/tailwind.config.js
+++ LP-1/tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["src/templates/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "bookmark-purple": "#5267DF",
+        "bookmark-red": "#FA5959",
+        "bookmark-blue": "#242445",
+        "bookmark-grey": "#9194A2",
+        "bookmark-white": "#F7F7F7",
+      }
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        lg: "1124px",
+        xl: "1124px",
+        "2xl": "1124px"
+      }
+    }
   },
   plugins: [],
 }
